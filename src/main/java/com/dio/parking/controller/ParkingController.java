ad7a94251209e9ef3c4ffb24793ede54be0c16b2
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-11-17 18:05:42 -0300

    Refactoring parking controller for encapsulated list in Response Entity

--- src/main/java/com/dio/parking/controller/ParkingController.java
+++ src/main/java/com/dio/parking/controller/ParkingController.java
@@ -2,6 +2,7 @@ package com.dio.parking.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +24,10 @@ public class ParkingController {
 	}
 
 	@GetMapping
-	public List<ParkingDTO> findAll() {
+	public ResponseEntity<List<ParkingDTO>> findAll() {
 		List<Parking> parkingList = parkingService.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-		return result;
+		return ResponseEntity.ok(result);
 
 	}
 }
